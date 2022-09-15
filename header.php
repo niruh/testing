73cc6bd4476b6c56643607bfb97e9e3aace63cd1
eridav <108908228+eridav@users.noreply.github.com>
2022-09-15 16:24:36 +1000

    initial commit

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testing</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+<div id="nav">
+    <div class="bg"></div>
+    <div class="logo box">
+        <a href="/"><img src="photos/reverse.png"></a>
+    </div>
+<!-- links start here -->
+
+<?php include("navigation.php")?> 
+
+    
+</div> <!-- end navigation bar -->
---
+++ navigation.php
@@ -0,0 +1,9 @@
+<div class="navi box">
+        <a href="/">About INTREC</a>
+        <a href="/">Capabilities</a>
+        <a href="/">Projects</a>
+        <a href="/">Commitments</a>
+        <a href="/">Careers</a>
+        <a href="/">Contact</a>
+        <a href="/">Subcontractors</a> 
+</div>
